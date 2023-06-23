680a883cadb1d54b8b0e15548bca1713d1b1d82c
jsipe091719 <molhw202000072@wmsu.edu.ph>
2023-06-23 22:25:37 +0700

    run-please

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/user/bin/env sh
+# abort on errors
+set -e
+
+#build
+npm run build
+
+#navigate into the build output directory
+echo > .nojekyll
+
+#if you are deploying to a custom domain
+#echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+#if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+#if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:jsipe091719/client.git main:gh-pages
+
+cd -
